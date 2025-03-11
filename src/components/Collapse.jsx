f22787e4908c20f02969387d834f054fb38b46cb
Brice <brettinb@hotmail.fr>
2025-03-11 13:55:48 +0100

    feat: add collapseData.json & apply to about page

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import PropTypes from 'prop-types';
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import PropTypes from "prop-types";
 
-function Collapse () {
+function Collapse ({ title, description }) {
     const [isOpen, setIsOpen] = useState(false);
 
     const handleCollapse = () => {
@@ -13,17 +13,22 @@ function Collapse () {
         <div className="collapse">
             <button className="collapse__button" onClick={handleCollapse}>
                 <div className="collapse__content">
-                    <span className="collapse__button-text">Fiabilité</span>
+                    <span className="collapse__button-text">{title}</span>
                     <span className="collapse__button-icon">{isOpen ? <FaChevronDown /> : <FaChevronUp />}</span>
                 </div>
             </button>
             {isOpen && (
                 <ul className="collapse__list">
-                    <li className="collapse__item">Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</li>
+                    <li className="collapse__item">{description}</li>
                 </ul>
             )}
         </div>
     );
 }
+
+Collapse.propTypes = {
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+};
 
 export default Collapse;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,18 @@
 import Banner from '../components/Banner';
 import Collapse from '../components/Collapse';
+import collapseData from '../data/collapseDate.json';
 
-function About () {
+function About() {
     return (
         <>
-        <Banner />
-        <Collapse />
+            <Banner />
+            <div className="about">
+                {collapseData.map(({ id, title, description }) => (
+                    <Collapse key={id} title={title} description={description} />
+                ))}
+            </div>
         </>
-    )
+    );
 }
 
 export default About;
